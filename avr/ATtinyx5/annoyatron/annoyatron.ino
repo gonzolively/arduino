ae8313b388fe662e836e11ec85018646ec746756
Knox Lively <knox.lively@protonmail.com>
2025-08-27 10:19:02 -0600

    minor tweaks

--- avr/ATtinyx5/annoyatron/annoyatron.ino
+++ avr/ATtinyx5/annoyatron/annoyatron.ino
@@ -8,48 +8,56 @@
 #include <stdint.h>
 #endif
 
-#define BODS 7                   //BOD Sleep bit in MCUCR
-#define BODSE 2                  //BOD Sleep enable bit in MCUCR
+#define BODS 7                         //BOD Sleep bit in MCUCR
+#define BODSE 2                        //BOD Sleep enable bit in MCUCR
 
-int PIN = 1;
-int REGULAR_HI_MS = 200;
-int WAKE_INDICATOR_HI_MS = 0; //200;
-int INITIAL_BEEP_COUNT = 3;   // number of "test" beeps before we go into the real loop
+const int PIN = 1;                     // PB1 for speaker output
+const int REGULAR_HI_MS = 200;         // Tone length
+const int WAKE_INDICATOR_HI_MS = 0;
+const int INITIAL_BEEP_COUNT = 3;      // number of "test" beeps before we go into the real loop
 
-// min/max number of 8-sec WDT periods to sleep for
-int RANDOM_SLEEP_MIN = 75;     // 10 minx (10 * 60 / 8)
-int RANDOM_SLEEP_MAX = 450;    // 60 mins (60 * 60 / 8)
+// Min/max number of 8-sec WDT periods to sleep for
+const int SLEEP_MIN = 75;              // 10 mins (10 * 60 / 8)
+const int SLEEP_MAX = 450;             // 60 mins (60 * 60 / 8)
+
+// Simple tone setting
+const int SIMPLE_TONE = 20;            // Fixed tone (classic "annoyatron" beep)
 
 uint8_t mcucr1, mcucr2;
-bool keepSleeping;                   //flag to keep sleeping or not
-unsigned long msNow;                 //the current time from millis()
-unsigned long msWakeUp;              //the time we woke up
-long wdtCount;                       //how many 8-sec WDT periods we've slept for
+bool keepSleeping;                     //flag to keep sleeping or not
+long wdtCount;                         //how many 8-sec WDT periods we've slept for
 
 void setup() {
-  for (int i=0; i < INITIAL_BEEP_COUNT - 1; i++) {
-    makeTone(REGULAR_HI_MS);
-    delay(100);
+  // Seed the random number generator with some pseudo-random value
+  randomSeed(TCNT1);
+
+  // Initial beeps to indicate startup
+  for (int i = 0; i < INITIAL_BEEP_COUNT; i++) {
+    makeTone(REGULAR_HI_MS, SIMPLE_TONE);
+    if (i < INITIAL_BEEP_COUNT - 1) {
+      delay(100);
+    }
   }
 }
 
 void loop() {
-  makeTone(REGULAR_HI_MS);
-  goToSleep(random(RANDOM_SLEEP_MIN, RANDOM_SLEEP_MAX + 1));
+  // Simple beep mode - consistent watch beep
+  makeTone(REGULAR_HI_MS, SIMPLE_TONE);
+  goToSleep(random(SLEEP_MIN, SLEEP_MAX + 1));
 }
 
-void makeTone(int msOfTone) {
+void makeTone(int msOfTone, int toneValue) {
   pinMode(PIN, OUTPUT);
-  startTone();
+  startTone(toneValue);
   delay(msOfTone);  // let the tone sound for a bit
   stopTone();
   pinMode(PIN, INPUT);
 }
 
-void startTone() {
-  // Original "annoyatron" tone, like a high pitch watch beep.
+void startTone(int toneValue) {
+  // Set up timer with specified tone value
   TCCR1 = 0x92;  // clock speed (highest to lowest values: 0x92 - 0x9F)
-  OCR1C = 20;   // Pitch (highest pitch to lowest: 18-)
+  OCR1C = toneValue;
 }
 
 void stopTone() {
@@ -59,17 +67,16 @@ void stopTone() {
 // wdtLimit = number of WDT periods to wake after
 void goToSleep(long wdtLimit)
 {
-    msNow = millis();
     wdtCount = 0;
-    
+
     do {
         ACSR |= _BV(ACD);                         //disable the analog comparator
         ADCSRA &= ~_BV(ADEN);                     //disable ADC
         set_sleep_mode(SLEEP_MODE_PWR_DOWN);
         sleep_enable();
-        
+
         wdtEnable();              //start the WDT
-        
+
         //turn off the brown-out detector.
         //must have an ATtiny45 or ATtiny85 rev C or later for software to be able to disable the BOD.
         //current while sleeping will be <0.5uA if BOD is disabled, <25uA if not.
@@ -84,20 +91,18 @@ void goToSleep(long wdtLimit)
         cli();                         //wake up here, disable interrupts
         sleep_disable();
         wdtDisable();                  //don't need the watchdog while we're awake
-        sei();                         //enable interrupts again (but INT0 is disabled above)
+        sei();                         //enable interrupts again
 
         if (++wdtCount < wdtLimit) {
             keepSleeping = true;
             if (WAKE_INDICATOR_HI_MS > 0) {
-              makeTone(WAKE_INDICATOR_HI_MS);            //briefly blink an LED so we can see the wdt wake-ups
+              makeTone(WAKE_INDICATOR_HI_MS, SIMPLE_TONE);
             }
         }
         else {
             keepSleeping = false;
         }
     } while (keepSleeping);
-    
-    msWakeUp = millis();
 }
 
 ISR(WDT_vect) {}                  //don't need to do anything here when the WDT wakes the MCU
@@ -112,7 +117,6 @@ void wdtEnable(void)
     WDTCR = _BV(WDIE) | _BV(WDP3) | _BV(WDP0);    //8192ms
     sei();
 }
-
 
 //disable the WDT
 void wdtDisable(void)
